74e716df1e1ad1878b09188435cc7b5be4d627ac
devwindsw <devwindsw@gmail.com>
2018-10-09 18:26:38 +0900

    Gather information on device

--- app/src/main/java/com/windsw/timezonehelper/MainActivity.java
+++ app/src/main/java/com/windsw/timezonehelper/MainActivity.java
@@ -1,8 +1,13 @@
 package com.windsw.timezonehelper;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.TimeUtils;
 import android.widget.TextView;
+
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String iso = tm.getNetworkCountryIso();
+        String numeric = tm.getNetworkOperator();
+        String name = tm.getNetworkOperatorName();
+
+        TimeZone zone = TimeZone.getDefault();
+        String version = TimeUtils.getTimeZoneDatabaseVersion();
+
+        String msg = "Welcome to " + name + "(" + numeric + ", " + iso + "), " + zone.getID() + "(" + version + ")";
+
         TextView tv = (TextView) findViewById(R.id.text_content);
-        tv.setText("Welcome!");
+        tv.setText(msg);
     }
 }
